4545d21ad5deed47bde34ecf687ae657a1056c21
Bryan Kim <bkim0826@gmail.com>
2022-04-03 14:10:34 -0400

    s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 // import { react } from "@babel/types";
-
+// import React from "react"
+import {useState} from 'react';
 function App() {
-  
+  const [calc,setCalc] = useState('')
+  const [result,setResut] = useState('')
+
+  const ops = ['/','*','+','-']
+  const updateCalc =value=>{
+    setCalc(calc + value);
+  }
   const handleClick=(e)=>{
       console.log('hello')
     }
@@ -13,6 +20,13 @@ function App() {
     }
     return nums;
   }
+  const handleOps = ()=>{
+    const jsxOps = []
+    for(let i = 0; i < ops.length; i++){
+      jsxOps.push(<button onClick={()=>updateCalc(ops[i])} key={i}>{ops[i]}</button>)
+    }
+    return jsxOps
+  }
   
   return (
     <div className="App">
@@ -20,18 +34,19 @@ function App() {
         Calcualtor!
       </header>
       <div className="calculator">
+        <div className='display'>
+          {result ? <span>(0)</span> : ''}{calc||'0'}
+        </div>
         <div className="base">
-          <div>
-            <button onClick={handleClick}>Clear</button>
-            <button>( )</button>
-            <button>/</button>
+          <div className="operators">
+            {handleOps()}
+            {/* <button>/</button>
             <button>*</button>
             <button>+</button>
             <button>-</button>
-            <button>=</button>
+            <button>=</button> */}
           </div>
-              
-            <div>
+            <div className="digits">
               {handleNumbers()}
             </div>
         </div>
