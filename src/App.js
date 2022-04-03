9003c76faf8c1d8e267ebae0147ff5e528573e1a
Bryan Kim <bkim0826@gmail.com>
2022-04-03 13:44:02 -0400

    d

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+// import { react } from "@babel/types";
 
 function App() {
+  
+  const handleClick=(e)=>{
+      console.log('hello')
+    }
 
   const handleNumbers = ()=>{
     const nums = []
     for(let i = 0; i<10;i++){
-      nums.push(<button key={i} className = {i}>{i}</button>)
+      nums.push(<button onClick = {handleClick}key={i} className = {i}>{i}</button>)
     }
     return nums;
   }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -16,10 +22,10 @@ function App() {
       <div className="calculator">
         <div className="base">
           <div>
-            <button>Clear</button>
+            <button onClick={handleClick}>Clear</button>
             <button>( )</button>
             <button>/</button>
-            <button>X</button>
+            <button>*</button>
             <button>+</button>
             <button>-</button>
             <button>=</button>
