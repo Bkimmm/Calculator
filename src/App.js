cef6c489dec6272b909bf12d9be956bb90736723
Bryan Kim <bkim0826@gmail.com>
2022-04-03 12:48:41 -0400

    s
    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+
 function App() {
+
+  const handleNumbers = ()=>{
+    const nums = []
+    for(let i = 0; i<10;i++){
+      nums.push(<button key={i} className = {i}>{i}</button>)
+    }
+    return nums;
+  }
   return (
     <div className="App">
       <header className="App-header">
         Calcualtor!
       </header>
+      <div className="calculator">
+        <div className="base">
+          <div>
+            <button>Clear</button>
+            <button>( )</button>
+            <button>/</button>
+            <button>X</button>
+            <button>+</button>
+            <button>-</button>
+            <button>=</button>
+          </div>
+              
+            <div>
+              {handleNumbers()}
+            </div>
+        </div>
+
+      </div>
     </div>
   );
 }
